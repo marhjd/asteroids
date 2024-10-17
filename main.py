bd638672a0283e7ab7ed3c4d5a7f028c3fc3337c
marhjd <aussenstelle+github@posteo.de>
2024-10-17 14:23:58 +0200

    Add spaceship (triangle) to the screen

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import pygame
+from player import Player
 from constants import *
 
 def main():
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
@@ -12,6 +14,7 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill(pygame.color.Color(0, 0, 0))
+        player.draw(screen)
         pygame.display.flip()
 
         # 60 fps
