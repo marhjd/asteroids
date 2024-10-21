85937dbccc97b15a4c83bf42202b39d05b525ead
marhjd <aussenstelle+github@posteo.de>
2024-10-21 09:41:53 +0200

    Add a pause between shooting for the player

--- player.py
+++ player.py
@@ -7,6 +7,7 @@ class Player(circleshape.CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, constants.PLAYER_RADIUS)
         self.rotation = 0
+        self.timer = 0
 
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -20,6 +21,9 @@ class Player(circleshape.CircleShape):
         self.rotation += constants.PLAYER_TURN_SPEED * dt
 
     def update(self, dt):
+        # reset shooting timer
+        self.timer -= dt
+
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
@@ -38,5 +42,8 @@ class Player(circleshape.CircleShape):
         self.position += forward * constants.PLAYER_SPEED * dt
     
     def shoot(self):
+        if self.timer > 0:
+            return
         shot = Shot(self.position.x, self.position.y, constants.SHOT_RADIUS)
         shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * constants.PLAYER_SHOOT_SPEED
+        self.timer = constants.PLAYER_SHOOT_COOLDOWN
